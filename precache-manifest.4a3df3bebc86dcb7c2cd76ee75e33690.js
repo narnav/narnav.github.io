0467890b9be1ecb3c7cd7a8b7c8de6ed4e60ea9b
UltraMik <eyalgo@017.net.il>
2020-06-10 10:37:00 +0300

    Updates

---
+++ precache-manifest.4a3df3bebc86dcb7c2cd76ee75e33690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea2fa84de2d397b708db17e191273bc",
+    "url": "/narnav.github.io/index.html"
+  },
+  {
+    "revision": "371e5f7e8c90ec47e53a",
+    "url": "/narnav.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "946842ace9794420d732",
+    "url": "/narnav.github.io/static/js/2.c1287597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/narnav.github.io/static/js/2.c1287597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371e5f7e8c90ec47e53a",
+    "url": "/narnav.github.io/static/js/main.c35a9856.chunk.js"
+  },
+  {
+    "revision": "3b3b634b84bb5ec8149b",
+    "url": "/narnav.github.io/static/js/runtime-main.39ab1521.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/narnav.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
