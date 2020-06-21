990597de5adf96ff9fb304a99f41390123a6710d
narnav <narnavg@gmail.com>
2020-06-21 14:42:07 +0300

    no red
    no red

---
+++ base.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1Bs2B1HOilC0w0JcwodaO1Tw9xEb1LQg",
+  authDomain: "ndsamp4.firebaseapp.com",
+  databaseURL: "https://ndsamp4.firebaseio.com",
+  projectId: "ndsamp4",
+  storageBucket: "ndsamp4.appspot.com",
+  messagingSenderId: "1009893056576",
+  appId: "1:1009893056576:web:de34fe2e9ae398613b7fa5",
+};
